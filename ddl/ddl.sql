7fd2a27d4a3802eae03d3741004900a9dfbf428b
satoru.kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-09-03 00:21:55 +0900

    followテーブルの作成

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -7,6 +7,7 @@ CREATE TABLE `account` (
   `header` text,
   `note` text,
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `delete_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -18,4 +19,15 @@ CREATE TABLE `status` (
   PRIMARY KEY (`id`),
   INDEX `idx_account_id` (`account_id`),
   CONSTRAINT `fk_status_account_id` FOREIGN KEY (`account_id`) REFERENCES  `account` (`id`)
+);
+
+CREATE TABLE `follow` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `follower_id` bigint(20) NOT NULL,
+  `followee_id` bigint(20) NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `delete_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_follower_id` (`follower_id`),
+  INDEX `idx_followee_id` (`followee_id`)
 );
