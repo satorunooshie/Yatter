6fa41d2131738355a50d4cd9f0606a20f85b8c03
satorunooshie <64164948+satorunooshie@users.noreply.github.com>
2021-09-18 23:35:44 +0900

    アカウント情報取得APIの作成 (#6)
    * アカウント情報取得APIの作成
    
    * add missing return

---
+++ app/handler/accounts/get.go
@@ -0,0 +1,39 @@
+package accounts
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/satorunooshie/Yatter/app/handler/httperror"
+
+	"github.com/go-chi/chi"
+)
+
+// Handle request for `GET /v1/accounts/{username}`
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	username := chi.URLParam(r, "username")
+	if username == "" {
+		httperror.BadRequest(w, errors.New("invalid params"))
+		return
+	}
+
+	ctx := r.Context()
+	accountRepo := h.app.Dao.Account() // domain/repository の取得
+
+	user, err := accountRepo.FindByUsername(ctx, username)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+	if user == nil {
+		httperror.BadRequest(w, errors.New("account does not exist"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(&user); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+}
