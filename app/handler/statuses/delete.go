4151d87df8b1d73029ad6eca814348ca47bfa2c1
satorunooshie <64164948+satorunooshie@users.noreply.github.com>
2021-09-23 01:30:16 +0900

    エラーハンドリングが足りてないところを修正 (#13)
    * AccountIDによる401バリデーションを追加
    
    * エラーハンドリングが足りてないところを修正
    
    * fix status.Account.ID
    
    * fix nil pointer

--- app/handler/statuses/delete.go
+++ app/handler/statuses/delete.go
@@ -2,6 +2,7 @@ package statuses
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/satorunooshie/Yatter/app/handler/auth"
@@ -19,18 +20,27 @@ func (h *handler) Delete(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	statusRepo := h.app.Dao.Status() // domain/repository の取得
 
-	accountID := auth.AccountOf(r).ID
+	account := auth.AccountOf(r)
+	if account == nil {
+		httperror.Error(w, http.StatusUnauthorized)
+		return
+	}
+
 	status, err := statusRepo.FindByID(ctx, id)
 	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
 	}
-	if status.Account.ID != accountID {
+	if status == nil {
+		httperror.BadRequest(w, errors.New("status does not exist"))
+		return
+	}
+	if status.AccountID != account.ID {
 		httperror.Error(w, http.StatusUnauthorized)
 		return
 	}
 
-	if err := statusRepo.Delete(ctx, id, accountID); err != nil {
+	if err := statusRepo.Delete(ctx, id, account.ID); err != nil {
 		httperror.InternalServerError(w, err)
 		return
 	}
